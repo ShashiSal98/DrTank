d39a769478d698338c73c8f475ec9641a740f3f9
ShashiSal98 <shashi.salwathura78@gmail.com>
2025-07-15 18:15:09 +0530

    Initial commit for DrTank

---
+++ send-mail.php
@@ -0,0 +1,26 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $to = "thimzlight@gmail.com";
+    $subject = "New Inquiry from Dr.Tank Website";
+
+    $name = htmlspecialchars($_POST["name"]);
+    $email = htmlspecialchars($_POST["email"]);
+    $phone = htmlspecialchars($_POST["phone"]);
+    $subjectInput = htmlspecialchars($_POST["subject"]);
+    $message = htmlspecialchars($_POST["message"]);
+
+    $body = "Name: $name\n";
+    $body .= "Email: $email\n";
+    $body .= "Phone: $phone\n";
+    $body .= "Subject: $subjectInput\n\n";
+    $body .= "Message:\n$message";
+
+    $headers = "From: $email";
+
+    if (mail($to, $subject, $body, $headers)) {
+        echo "Message sent successfully.";
+    } else {
+        echo "Message failed to send.";
+    }
+}
+?>
